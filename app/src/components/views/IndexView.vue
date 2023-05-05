afc9dd0f809245d889c9df2b0bafae8e8009b3ca
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 15:51:51 +0200

    changes on indexview

--- app/src/components/views/IndexView.vue
+++ app/src/components/views/IndexView.vue
@@ -11,18 +11,16 @@
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { RouterView } from "vue-router";
 import appHeader from "@/components/modules/appHeader.vue";
 import appFooter from "@/components/modules/appFooter.vue";
 
 export default defineComponent({
   name: "IndexView",
-  components: { appHeader, appFooter, RouterView },
+  components: { appHeader, appFooter },
   setup() {
     return {
       appHeader,
       appFooter,
-      RouterView,
     };
   },
 });
