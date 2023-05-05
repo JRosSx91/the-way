93ba16957918ed0e74575427e1c5ffc8b2d81dec
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 16:09:32 +0200

    add styles to header

--- app/src/components/modules/appHeader.vue
+++ app/src/components/modules/appHeader.vue
@@ -1,10 +1,12 @@
 <template>
   <header id="header" class="flex flex-row justify-start m-0 p-1 bg-[#607d8b]">
     <section
-      class="dark-light-btn bg-[#455a64] border-0 text-white cursor-pointer"
+      class="dark-light-btn bg-[#455a64] border-0 text-white cursor-pointer no-underline"
     ></section>
     <h1 class="title align-middle text-white mr-1 text-xl font-bold"></h1>
-    <nav class="navigation">
+    <nav
+      class="navigation dark-light-btn bg-[#455a64] border-0 text-white cursor-pointer no-underline"
+    >
       <ul>
         <li></li>
         <li></li>
@@ -18,3 +20,13 @@ import { defineComponent } from "vue";
 
 export default defineComponent({});
 </script>
+<style lang="scss" scoped>
+#header {
+  font-family: "Roboto", sans-serif;
+
+  & .navigation {
+    box-shadow: 0 0 0 0.5px rgba(49, 49, 93, 0.03),
+      0 2px 5px 0 rgba(49, 49, 93, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.08);
+  }
+}
+</style>
