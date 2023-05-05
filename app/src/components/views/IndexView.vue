f662a50fc9056eb96c17aa823d761ec30216906d
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 15:00:39 +0200

    import components on index

--- app/src/components/views/IndexView.vue
+++ app/src/components/views/IndexView.vue
@@ -3,4 +3,13 @@
   <router-view />
   <appFooter />
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+import appHeader from "@/components/modules/appHeader";
+import appFooter from "@/components/modules/appFooter";
+
+export default defineComponent({
+  name: "IndexView",
+});
+</script>
