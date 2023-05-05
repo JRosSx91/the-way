ee03377900d0f894194368121c53d7cea0523f84
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 16:57:53 +0200

    adding button on nav for mobile devices

--- app/src/components/modules/appHeader.vue
+++ app/src/components/modules/appHeader.vue
@@ -6,23 +6,36 @@
       <button>Test 1</button>
       <button>Test 2</button>
     </section>
-    <h1 class="title align-middle text-white mr-1 text-xl font-bold">Lorem</h1>
+    <h1 class="title align-middle justify-center text-white text-xl font-bold">
+      Lorem
+    </h1>
     <nav
       class="navigation dark-light-btn bg-[#455a64] border-0 text-white cursor-pointer no-underline"
     >
-      <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+      <button v-if="store.phone"></button>
+      <ul v-else class="flex">
+        <li>
+          <a><p>Test 1</p></a>
+        </li>
+        <li>
+          <a><p>Test 2</p></a>
+        </li>
+        <li>
+          <a><p>Test 3</p></a>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import useStore from "@/services/store";
 
 export default defineComponent({
   name: "app-header",
+  setup() {
+    const store = useStore();
+  },
 });
 </script>
 <style lang="scss" scoped>
