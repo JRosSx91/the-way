0a55d9cb5922583a094410eeb6e9dd6039e442da
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 15:42:59 +0200

    add styles for webkit scroll bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -6,12 +6,35 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  el: "App",
+  name: "App",
   components: {},
 });
 </script>
 
 <style>
+::-webkit-scrollbar {
+  width: 3px;
+  height: 3px;
+  background-color: transparent !important;
+}
+
+::-webkit-scrollbar-track {
+  background-color: rgba(26, 26, 26, 0.692) !important;
+  border-radius: 1rem;
+  margin: 0.4rem;
+  margin-top: 0.4rem;
+  border-left: 1px solid rgba(211, 211, 211, 0) !important;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 20px;
+  background: linear-gradient(
+    140deg,
+    rgba(0, 0, 0, 0.384),
+    #00000031
+  ) !important;
+  box-shadow: inset 0 0 6px rgb(0, 0, 0) !important;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
