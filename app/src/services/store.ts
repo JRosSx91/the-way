fe87b2fb66adfe73b6b8a05f5b364f2ad77790c3
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 14:15:20 +0200

    define and export store on store.ts

--- app/src/services/store.ts
+++ app/src/services/store.ts
@@ -1,4 +1,12 @@
 import { defineStore } from "pinia";
-import State from "@/interfaces/index";
+import { State } from "@/interfaces/index";
 
+const useStore = defineStore("store", {
+  state: (): State => {
+    return {
+      dark: false,
+    };
+  },
+});
 
+export default useStore;
