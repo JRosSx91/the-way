e3be0c9033d8ced313fe8614aeb1246c6e2884ba
JRosSx91 <hazee.1491@gmail.com>
2023-05-05 16:11:04 +0200

    add styles to header

--- app/src/components/modules/appHeader.vue
+++ app/src/components/modules/appHeader.vue
@@ -18,7 +18,9 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  name: "app-header",
+});
 </script>
 <style lang="scss" scoped>
 #header {
