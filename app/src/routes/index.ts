015a780256250d50d1cd4a7daf18ddc2e2382be4
JRosSx91 <hazee.1491@gmail.com>
2023-05-07 16:58:06 +0200

    updating routes to views

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,15 +1,9 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import WelcomeView from "@/components/views/WelcomeView.vue";
-import IndexView from "@/components/views/IndexView.vue";
+import IndexView from "@/views/IndexView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "welcome",
-    component: WelcomeView,
-  },
-  {
-    path: "/index",
     name: "index",
     component: IndexView,
   },
