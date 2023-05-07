e27230a1fae8eb51326f42bef7e8c89f3ea0a9fd
JRosSx91 <hazee.1491@gmail.com>
2023-05-07 16:19:31 +0200

    changing links on index view

--- app/src/views/IndexView.vue
+++ app/src/views/IndexView.vue
@@ -1,25 +1,25 @@
 <template>
-  <appHeader />
+  <header-layout />
   <citiesComp :images="[img1, img2, img3, img4]" />
-  <appFooter />
+  <footer-layout />
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import appHeader from "@/components/modules/appHeader.vue";
-import appFooter from "@/components/modules/appFooter.vue";
+import FooterLayout from "@/layouts/FooterLayout.vue";
+import HeaderLayout from "@/layouts/HeaderLayout.vue";
 import citiesComp from "../modules/citiesComp.vue";
 
 export default defineComponent({
   name: "IndexView",
-  components: { appHeader, appFooter, citiesComp },
+  components: { HeaderLayout, FooterLayout, citiesComp },
   setup() {
     return {
       img1: require("@/assets/img/leopard2.jpg"),
       img2: require("@/assets/img/lion2.jpg"),
       img3: require("@/assets/img/tiger2.jpg"),
       img4: require("@/assets/img/bear2.jpg"),
-      appHeader,
-      appFooter,
+      HeaderLayout,
+      FooterLayout,
       citiesComp,
     };
   },
