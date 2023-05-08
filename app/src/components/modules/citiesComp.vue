9df3301576f7879243f099a727c2924123ce8744
JRosSx91 <hazee.1491@gmail.com>
2023-05-08 14:26:36 +0200

    adding ts functions

--- app/src/components/modules/citiesComp.vue
+++ app/src/components/modules/citiesComp.vue
@@ -42,12 +42,254 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import * as THREE from "three";
 import gsap from "gsap";
 import imagesLoaded from "imagesloaded";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    let activeSlideId = 0;
+    onMounted(() => {
+      const displacementSlider = function (opts: any) {
+        let vertex = `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }
+`;
+
+        let fragment = `
+    
+    varying vec2 vUv;
+
+    uniform sampler2D currentImage;
+    uniform sampler2D nextImage;
+
+    uniform float dispFactor;
+
+    void main() {
+
+        vec2 uv = vUv;
+        vec4 _currentImage;
+        vec4 _nextImage;
+        float intensity = 0.3;
+
+        vec4 orig1 = texture2D(currentImage, uv);
+        vec4 orig2 = texture2D(nextImage, uv);
+        
+        _currentImage = texture2D(currentImage, vec2(uv.x, uv.y + dispFactor * (orig2 * intensity)));
+
+        _nextImage = texture2D(nextImage, vec2(uv.x, uv.y + (1.0 - dispFactor) * (orig1 * intensity)));
+
+        vec4 finalTexture = mix(_currentImage, _nextImage, dispFactor);
+
+        gl_FragColor = finalTexture;
+
+    }
+`;
+
+        let images = opts.images,
+          image,
+          sliderImages: any[] = [];
+        let canvasWidth = images[0].clientWidth;
+        let canvasHeight = images[0].clientHeight;
+        let parent = opts.parent;
+        let renderWidth = Math.max(
+          document.documentElement.clientWidth,
+          window.innerWidth || 0
+        );
+        let renderHeight = Math.max(
+          document.documentElement.clientHeight,
+          window.innerHeight || 0
+        );
+
+        let renderW: any, renderH: any;
+
+        if (renderWidth > canvasWidth) {
+          renderW = renderWidth;
+        } else {
+          renderW = canvasWidth;
+        }
+
+        renderH = canvasHeight;
+
+        let renderer = new THREE.WebGLRenderer({
+          antialias: false,
+        });
+
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setClearColor(0x23272a, 1.0);
+        renderer.setSize(renderW, renderH);
+        parent.appendChild(renderer.domElement);
+
+        let loader = new THREE.TextureLoader();
+        loader.crossOrigin = "anonymous";
+
+        images.forEach((img: any) => {
+          image = loader.load(img.getAttribute("src") + "?v=" + Date.now());
+          image.magFilter = image.minFilter = THREE.LinearFilter;
+          image.anisotropy = renderer.capabilities.getMaxAnisotropy();
+          sliderImages.push(image);
+        });
+
+        let scene = new THREE.Scene();
+        scene.background = new THREE.Color(0x23272a);
+        let camera = new THREE.OrthographicCamera(
+          renderWidth / -2,
+          renderWidth / 2,
+          renderHeight / 2,
+          renderHeight / -2,
+          1,
+          1000
+        );
+
+        camera.position.z = 1;
+
+        let mat = new THREE.ShaderMaterial({
+          uniforms: {
+            dispFactor: { value: 0.0 },
+            currentImage: { value: sliderImages[0] },
+            nextImage: { value: sliderImages[1] },
+          },
+          vertexShader: vertex,
+          fragmentShader: fragment,
+          transparent: true,
+          opacity: 1.0,
+        });
+
+        let geometry = new THREE.PlaneGeometry(
+          parent.offsetWidth,
+          parent.offsetHeight,
+          1
+        );
+        let object = new THREE.Mesh(geometry, mat);
+        object.position.set(0, 0, 0);
+        scene.add(object);
+
+        const addEvents = function () {
+          const paginationElement = document.getElementById("pagination");
+
+          if (paginationElement !== null) {
+            const pagButtons = Array.from(
+              paginationElement.querySelectorAll("button")
+            );
+            let isAnimating = false;
+
+            pagButtons.forEach((el) => {
+              el.addEventListener("click", function () {
+                if (!isAnimating) {
+                  isAnimating = true;
+
+                  paginationElement.querySelectorAll(".active")[0].className =
+                    "";
+                  this.className = "active";
+
+                  const slideIdString = this.dataset.slide;
+
+                  if (slideIdString !== undefined) {
+                    const slideId = parseInt(slideIdString, 10);
+
+                    activeSlideId = slideId; // <-- Actualizar el índice de la imagen activa
+
+                    mat.uniforms.nextImage.value = sliderImages[slideId];
+                    mat.uniforms.nextImage.needsUpdate = true;
+
+                    gsap.to(mat.uniforms.dispFactor, {
+                      duration: 1,
+                      value: 1,
+                      ease: "Expo.easeInOut",
+                      onComplete: function () {
+                        mat.uniforms.currentImage.value = sliderImages[slideId];
+                        mat.uniforms.currentImage.needsUpdate = true;
+                        mat.uniforms.dispFactor.value = 0.0;
+                        isAnimating = false;
+                      },
+                    });
+                    const slideTitleEl = document.getElementById("slide-title");
+                    const slideStatusEl =
+                      document.getElementById("slide-status");
+                    const nextSlideTitle = document.querySelectorAll(
+                      `[data-slide-title="${slideId}"]`
+                    )[0].innerHTML;
+                    const nextSlideStatus = document.querySelectorAll(
+                      `[data-slide-status="${slideId}"]`
+                    )[0].innerHTML;
+
+                    gsap.fromTo(
+                      slideTitleEl,
+                      {
+                        duration: 0.5,
+                        autoAlpha: 1,
+                        y: 0,
+                      },
+                      {
+                        autoAlpha: 0,
+                        y: 20,
+                        ease: "Expo.easeIn",
+                        onComplete: function () {
+                          if (slideTitleEl) {
+                            slideTitleEl.innerHTML = nextSlideTitle;
+
+                            gsap.to(slideTitleEl, {
+                              duration: 0.5,
+                              autoAlpha: 1,
+                              y: 0,
+                            });
+                          }
+                        },
+                      }
+                    );
+
+                    gsap.fromTo(
+                      slideStatusEl,
+                      {
+                        duration: 0.5,
+                        autoAlpha: 1,
+                        y: 0,
+                      },
+                      {
+                        autoAlpha: 0,
+                        y: 20,
+                        ease: "Expo.easeIn",
+                        onComplete: function () {
+                          if (slideStatusEl) {
+                            slideStatusEl.innerHTML = nextSlideStatus;
+
+                            gsap.to(slideStatusEl, {
+                              duration: 0.5,
+                              autoAlpha: 1,
+                              y: 0,
+                              delay: 0.1,
+                            });
+                          }
+                        },
+                      }
+                    );
+                  }
+                }
+              });
+            });
+          }
+
+          addEvents();
+
+          window.addEventListener("resize", function (e) {
+            renderer.setSize(renderW, renderH);
+          });
+
+          let animate = function () {
+            requestAnimationFrame(animate);
+
+            renderer.render(scene, camera);
+          };
+          animate();
+        };
+      };
+    });
+  },
+});
 </script>
 
 <style lang="scss" scoped>
