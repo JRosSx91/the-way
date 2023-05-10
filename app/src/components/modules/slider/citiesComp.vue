aad4a51ffb40aad39dbb45c940f68fe213e8099e
JRosSx91 <hazee.1491@gmail.com>
2023-05-10 23:29:37 +0200

    change functions on cities components

--- app/src/components/modules/slider/citiesComp.vue
+++ app/src/components/modules/slider/citiesComp.vue
@@ -47,56 +47,14 @@ import gsap from "gsap";
 import imagesLoaded from "imagesloaded";
 import { DisplacementSliderOptions, Slide } from "@/interfaces";
 import useStore from "@/store";
+import { slidesData } from "./slidesData";
 
 export default defineComponent({
   setup() {
     const store = useStore();
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let activeSlideId = 0;
-    const slides = ref<Slide[]>([
-      {
-        title: "Pamplona",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/pamplona-dia.jpg"),
-        imgNight: require("@/assets/img/pamplona-noche.jpeg"),
-      },
-      {
-        title: "Logroño",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/logroño-dia.jpg"),
-        imgNight: require("@/assets/img/logroño-noche.jpg"),
-      },
-      {
-        title: "Burgos",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/burgos-dia.jpg"),
-        imgNight: require("@/assets/img/burgos-noche.webp"),
-      },
-      {
-        title: "León",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/leon-dia.jpg"),
-        imgNight: require("@/assets/img/leon-noche.jpg"),
-      },
-      {
-        title: "Astorga",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/astorga-dia.jpg"),
-        imgNight: require("@/assets/img/astorga-noche.jpg"),
-      },
-      {
-        title: "Ponferrada",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/ponferrada-dia.webp"),
-        imgNight: require("@/assets/img/ponferrada-noche.jpg"),
-      },
-      {
-        title: "Santiago de Compostela",
-        status: "Lorem Ipsum",
-        img: require("@/assets/img/santiago.webp"),
-        imgNight: require("@/assets/img/santiago-noche.jpg"),
-      },
-    ]);
+    const slides = ref<Slide[]>(slidesData);
     const displacementSlider = function (opts: DisplacementSliderOptions) {
       let isAnimating = false;
       let vertex = `
