19aa78812912c43625737bcb8765cc869aa9145c
JRosSx91 <hazee.1491@gmail.com>
2023-05-10 19:00:14 +0200

    create title comp

---
+++ app/src/components/common/titleComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mx-auto">
+    <h1>W<span>AI</span></h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+h1 {
+  color: white;
+
+  span {
+    color: red;
+  }
+}
+</style>
