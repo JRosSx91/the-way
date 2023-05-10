982529c5aa77c182d2614d490545a392a1ba3e16
JRosSx91 <hazee.1491@gmail.com>
2023-05-10 23:27:58 +0200

    move slide data to a infividual ts

--- app/src/views/IndexView.vue
+++ app/src/views/IndexView.vue
@@ -8,8 +8,8 @@
 import { defineComponent } from "vue";
 import FooterLayout from "@/layouts/FooterLayout.vue";
 import HeaderLayout from "@/layouts/HeaderLayout.vue";
-import citiesComp from "@/components/modules/citiesComp.vue";
-import nightCities from "@/components/modules/nightCities.vue";
+import citiesComp from "@/components/modules/slider/citiesComp.vue";
+import nightCities from "@/components/modules/slider/nightCities.vue";
 import useStore from "@/store";
 
 export default defineComponent({
