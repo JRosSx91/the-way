c7c846a97d3c7420e6804a4a7561243f1d13e275
JRosSx91 <hazee.1491@gmail.com>
2023-05-11 15:23:03 +0200

    create main layout

---
+++ app/src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <headerComp />
+  <main>
+    <slot></slot>
+  </main>
+  <footerComp />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "main-layout",
+});
+</script>
--- app/src/views/IndexView.vue
+++ app/src/views/IndexView.vue
@@ -1,25 +1,20 @@
 <template>
-  <header-layout />
-  <citiesComp v-if="!store.dark" />
-  <nightCities v-else />
-  <footer-layout />
+  <MainLayout></MainLayout>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import FooterLayout from "@/layouts/FooterLayout.vue";
-import HeaderLayout from "@/layouts/HeaderLayout.vue";
+import MainLayout from "@/layouts/MainLayout.vue";
 import citiesComp from "@/components/modules/slider/citiesComp.vue";
 import nightCities from "@/components/modules/slider/nightCities.vue";
 import useStore from "@/store";
 
 export default defineComponent({
   name: "IndexView",
-  components: { HeaderLayout, FooterLayout, citiesComp, nightCities },
+  components: { MainLayout, citiesComp, nightCities },
   setup() {
     const store = useStore();
     return {
-      HeaderLayout,
-      FooterLayout,
+      MainLayout,
       citiesComp,
       nightCities,
       store,
