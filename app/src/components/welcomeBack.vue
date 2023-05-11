eaab0c1cf2bf241bea175bd3517638da4aa728ac
JRosSx91 <hazee.1491@gmail.com>
2023-05-11 16:37:52 +0200

    create background component

---
+++ app/src/components/welcomeBack.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="new-page"></div>
+  <div class="old-page"></div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "welcome-back",
+});
+</script>
+
