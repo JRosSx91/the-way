d4b16d21cfd7293b03cd4852813fb463deaf6184
JRosSx91 <hazee.1491@gmail.com>
2023-05-12 15:13:41 +0200

    add route to wayview

--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import IndexView from "@/views/IndexView.vue";
 import WayView from "@/views/WayView.vue";
+import HistoricView from "@/views/HistoricView.vue";
+import AboutMe from "@/views/AboutMe.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +14,16 @@ const routes: Array<RouteRecordRaw> = [
     path: "/way",
     name: "ways",
     component: WayView,
+  },
+  {
+    path: "/past",
+    name: "past",
+    component: HistoricView,
+  },
+  {
+    path: "/me",
+    name: "me",
+    component: AboutMe,
   },
 ];
 
