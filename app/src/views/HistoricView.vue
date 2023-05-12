7855fd42ea713500dce365fa84936454294b185d
JRosSx91 <hazee.1491@gmail.com>
2023-05-12 15:17:02 +0200

    create wayview

---
+++ app/src/views/HistoricView.vue
@@ -0,0 +1,20 @@
+<template>
+  <MainLayout> </MainLayout>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import MainLayout from "@/layouts/MainLayout.vue";
+import useStore from "@/store";
+
+export default defineComponent({
+  name: "IndexView",
+  components: { MainLayout },
+  setup() {
+    const store = useStore();
+    return {
+      MainLayout,
+      store,
+    };
+  },
+});
+</script>
