9304d7e52908054f8e7e48cd3e07caf326cf1e38
JRosSx91 <hazee.1491@gmail.com>
2023-05-15 15:30:26 +0200

    add script to socialview

--- app/src/views/SocialView.vue
+++ app/src/views/SocialView.vue
@@ -1,3 +1,16 @@
 <template>
   <socialLayout></socialLayout>
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import SocialLayout from "@/layouts/SocialLayout.vue";
+
+export default defineComponent({
+  components: { SocialLayout },
+  setup() {
+    return {
+      SocialLayout,
+    };
+  },
+});
+</script>
