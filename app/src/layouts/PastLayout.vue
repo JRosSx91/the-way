5986a83697da2113f606c27243bc2773d1fb7a2d
JRosSx91 <hazee.1491@gmail.com>
2023-05-15 16:02:28 +0200

    add script to past layout

--- app/src/layouts/PastLayout.vue
+++ app/src/layouts/PastLayout.vue
@@ -1,5 +1,21 @@
 <template>
-  <HistoricHeader />
+  <historicHeader />
   <slot></slot>
   <pastFooter />
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import historicHeader from "@/components/historicHeader.vue";
+import pastFooter from "@/components/pastFooter.vue";
+
+export default defineComponent({
+  name: "past-layout",
+  components: { historicHeader, pastFooter },
+  setup() {
+    return {
+      historicHeader,
+      pastFooter,
+    };
+  },
+});
+</script>
