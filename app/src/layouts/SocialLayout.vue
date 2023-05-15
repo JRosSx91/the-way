c3259f07244998b3c95ade057546a28faa407971
JRosSx91 <hazee.1491@gmail.com>
2023-05-15 16:05:47 +0200

    add script to social layout

--- app/src/layouts/SocialLayout.vue
+++ app/src/layouts/SocialLayout.vue
@@ -4,3 +4,22 @@
   <slot></slot>
   <socialFooter />
 </template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import socialHeader from "@/components/historicHeader.vue";
+import socialFooter from "@/components/pastFooter.vue";
+import socialMenu from "@/components/socialMenu.vue";
+
+export default defineComponent({
+  name: "past-layout",
+  components: { socialHeader, socialMenu, socialFooter },
+  setup() {
+    return {
+      socialHeader,
+      socialMenu,
+      socialFooter,
+    };
+  },
+});
+</script>
+
