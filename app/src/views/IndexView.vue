933bc879f7b22e59d539f4d90ae612f879a0c97a
JRosSx91 <hazee.1491@gmail.com>
2023-05-15 16:12:45 +0200

    deleting welcome layout from index view

--- app/src/views/IndexView.vue
+++ app/src/views/IndexView.vue
@@ -1,6 +1,5 @@
 <template>
-  <WelcomeLayout v-if="store.login" />
-  <MainLayout v-else>
+  <MainLayout>
     <citiesComp v-if="!store.dark" />
     <nightCities v-else />
   </MainLayout>
@@ -8,21 +7,19 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import MainLayout from "@/layouts/MainLayout.vue";
-import WelcomeLayout from "@/layouts/WelcomeLayout.vue";
 import useStore from "@/store";
 import citiesComp from "@/modules/slider/citiesComp.vue";
 import nightCities from "@/modules/slider/nightCities.vue";
 
 export default defineComponent({
   name: "IndexView",
-  components: { MainLayout, citiesComp, nightCities, WelcomeLayout },
+  components: { MainLayout, citiesComp, nightCities },
   setup() {
     const store = useStore();
     return {
       MainLayout,
       citiesComp,
       nightCities,
-      WelcomeLayout,
       store,
     };
   },
