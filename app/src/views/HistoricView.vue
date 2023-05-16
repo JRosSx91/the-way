3bb5bb5652fe8cc79160a3bbeeb643c50a69650d
JRosSx91 <hazee.1491@gmail.com>
2023-05-16 15:05:25 +0200

    add component on view

--- app/src/views/HistoricView.vue
+++ app/src/views/HistoricView.vue
@@ -1,18 +1,22 @@
 <template>
-  <PastLayout> </PastLayout>
+  <PastLayout>
+    <PastWays />
+  </PastLayout>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import PastLayout from "@/layouts/PastLayout.vue";
 import useStore from "@/store";
+import PastWays from "@/components/pastWays.vue";
 
 export default defineComponent({
   name: "IndexView",
-  components: { PastLayout },
+  components: { PastLayout, PastWays },
   setup() {
     const store = useStore();
     return {
       PastLayout,
+      PastWays,
       store,
     };
   },
